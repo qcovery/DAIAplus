34add186d82c4b4f97bdf0e3901869d4ef0f562f
Johannes Schultze <schultze@effective-webwork.de>
2024-09-12 07:16:25 +0200

    Bugfixes DAIAplus.

--- config/module.config.php
+++ config/module.config.php
@@ -6,21 +6,29 @@ $config = [
         'allow_override' => true,
         'aliases' => [
             'daia' => 'DAIAplus\ILS\Driver\DAIA',
-            'getArticleStatuses' => 'DAIAplus\AjaxHandler\GetArticleStatuses',
-            'getArticleStatusesUW' => 'DAIAplus\AjaxHandler\GetArticleStatusesUW',
-            'getItemStatuses' => 'DAIAplus\AjaxHandler\GetItemStatuses',
             'paia' => 'DAIAplus\ILS\Driver\PAIA',
             'VuFind\ILSConnection' => 'DAIAplus\ILS\Connection',
         ],
         'factories' => [
             'DAIAplus\ILS\Driver\DAIA' => 'DAIAplus\ILS\Driver\DriverWithDateConverterFactory',
             'DAIAplus\ILS\Driver\PAIA' => 'DAIAplus\ILS\Driver\PAIAFactory',
-
-
             'DAIAplus\ILS\Connection' => 'DAIAplus\ILS\ConnectionFactory',
-            'DAIAplus\AjaxHandler\GetArticleStatuses' => 'DAIAplus\AjaxHandler\GetArticleStatusesFactory',
-            'DAIAplus\AjaxHandler\GetArticleStatusesUW' => 'DAIAplus\AjaxHandler\GetArticleStatusesFactoryUW',
-            'DAIAplus\AjaxHandler\GetItemStatuses' => 'DAIAplus\AjaxHandler\GetItemStatusesFactory',
+        ],
+    ],
+    'vufind' => [
+        'plugin_managers' => [
+            'ajaxhandler' => [
+                'factories' => [
+                    'DAIAplus\AjaxHandler\GetArticleStatuses' => 'DAIAplus\AjaxHandler\GetArticleStatusesFactory',
+                    'DAIAplus\AjaxHandler\GetArticleStatusesUW' => 'DAIAplus\AjaxHandler\GetArticleStatusesFactoryUW',
+                    //'DAIAplus\AjaxHandler\GetItemStatuses' => 'DAIAplus\AjaxHandler\GetItemStatusesFactory',
+                ],
+                'aliases' => [
+                    'getArticleStatuses' => 'DAIAplus\AjaxHandler\GetArticleStatuses',
+                    'getArticleStatusesUW' => 'DAIAplus\AjaxHandler\GetArticleStatusesUW',
+                    //'getItemStatuses' => 'DAIAplus\AjaxHandler\GetItemStatuses',
+                ],
+            ],
         ],
     ],
 ];
